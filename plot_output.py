9c5e6388e98ae097e1ae99bc4cd00d2d8f753693
Aaron D. Milstein <neurosutras@gmail.com>
2019-11-20 14:46:37 -0800

    20191120:  - In full model, virtual cells do not have a model_name attribute, but they do have a _population attribute.

--- plot_output.py
+++ plot_output.py
@@ -1,8 +1,26 @@
 from bmtk.analyzer.visualization.spikes import plot_raster
 import matplotlib.pyplot as plt
+import click
 
-# Raster plot of the v1 spikes.
-plt.figure('Raster')
-plot_raster('output/spikes.h5', with_histogram=True, with_labels=['l4'], group_by='model_name', show_plot=False,
-            nodes_file='toy_network/l4_nodes.h5', node_types_file='toy_network/l4_node_types.csv')
-plt.show()
+
+@click.command()
+@click.option("--spikes-file-path", type=click.Path(exists=True, file_okay=True, dir_okay=False), required=True)
+@click.option("--with-histogram", type=bool, default=True)
+@click.option("--with-labels", multiple=True, type=str, default=['l4'])
+@click.option("--group-by", type=str, default='model_name')
+@click.option("--nodes-file-path", type=click.Path(exists=True, file_okay=True, dir_okay=False), required=True)
+@click.option("--node-types-file-path", type=click.Path(exists=True, file_okay=True, dir_okay=False), required=True)
+def main(spikes_file_path, with_histogram, with_labels, group_by, nodes_file_path, node_types_file_path):
+    """
+    Generates spike raster plot from L4 simulation output.
+    :param spikes_file_path: str (path)
+    :param with_histogram: bool
+    :param with_labels: list of str
+    :param group_by: str
+    :param nodes_file_path: str (path)
+    :param node_types_file_path: str (path)
+    """
+    plt.figure('Raster')
+    plot_raster(spikes_file_path, with_histogram=with_histogram, with_labels=with_labels, group_by=group_by,
+                show_plot=False, nodes_file=nodes_file_path, node_types_file=node_types_file_path)
+    plt.show()
